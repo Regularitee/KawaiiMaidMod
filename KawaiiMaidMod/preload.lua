ea0bab580fbd0e5b1e95f3d7c71d9adce9228dc7
Regularitee <yoursubjectnamehere@gmail.com>
2018-08-31 01:57:47 -0600

    Add files via upload

--- KawaiiMaidMod/preload.lua
+++ KawaiiMaidMod/preload.lua
@@ -60,15 +60,15 @@ local reward_list = {
 
 -- ■素材系アイテム表示名,id,cost,rank --序盤はポイントが重すぎてなんらかの逼迫した状況専用そう。R5あたりからAPとチャージ量と係数分で現実的に…と思ったらRank5からポイント需要が…
 local material_list = {
-	"ビニール袋", "bag_plastic", 100, 2,
-	"木材", "2x4", 500, 2,
-	"プラスチック片", "plastic_chunk", 500, 2,
-	"端切れ(革)", "leather", 500, 2, 
-	"布", "rag", 500, 2,
-	"糸", "thread", 500, 2,
-	"鉄屑", "scrap", 500, 2,
-	"ケブラー", "kevlar_plate", 1000, 4,
-	"ノーメックス", "nomex", 1000, 4
+	"plastic bags", "bag_plastic", 100, 2,
+	"wood planks", "2x4", 500, 2,
+	"plastic piece", "plastic_chunk", 500, 2,
+	"leather", "leather", 500, 2, 
+	"cloth rags", "rag", 500, 2,
+	"thread", "thread", 500, 2,
+	"scrap metal", "scrap", 500, 2,
+	"kevlar plates", "kevlar_plate", 1000, 4,
+	"nomex", "nomex", 1000, 4
 }
 
 -- ■装備系アイテムid,cost,rank --消耗品系の用途が欲しい
@@ -136,10 +136,10 @@ DNr = "kawaii_amts_reciver"
 DNt = "kawaii_amts_transmitter"
 
 -- ■未解放時のメニュー表記
-local equipStr = "<color_dark_gray>[[ロックされています]]:解放Rank2</color>"
-local materialStr = "<color_dark_gray>[[ロックされています]]:解放Rank2</color>"
-local liquidStr = "<color_dark_gray>[[ロックされています]]:解放Rank3</color>"
-local armsStr = "<color_dark_gray>[[ロックされています]]:解放Rank5</color>"
+local equipStr = "<color_dark_gray>[[Locked]]: Requires Rank2</color>"
+local materialStr = "<color_dark_gray>[[Locked]]: Requires Rank2</color>"
+local liquidStr = "<color_dark_gray>[[Locked]]: Requires Rank3</color>"
+local armsStr = "<color_dark_gray>[[Locked]]: Requires Rank5</color>"
 
 
 -- ■転送要請(アイテム入手)アイテムの処理
@@ -148,23 +148,23 @@ function amts_reciver(item2, active)
 	EditRP(0)
 
 	if AMTS_Rank > 1 then
-		equipStr = "装備転送"
-		materialStr = "素材転送"
+		equipStr = "Buy a product"
+		materialStr = "Sell resources"
 	end
 	if AMTS_Rank > 2 then
-		liquidStr = "液体転送"
+		liquidStr = "Liquid Transfer"
 	end
 	if AMTS_Rank > 4 then
-		armsStr = "AM-ARMSコントロール"
+		armsStr = "AM-ARMS Controller"
 	end
 	
-	local c = CreateMenu("AMTS-転送(所有ポイント:" .. AMTS_Point .. "/Rank:" .. AMTS_Rank .. "/Next:" .. AMTS_NextRP ..")" , armsStr, "物資転送", materialStr, liquidStr, equipStr, "やめる")
+	local c = CreateMenu("AMTS Product Catalogue (Points:" .. AMTS_Point .. "/Rank:" .. AMTS_Rank .. "/Next:" .. AMTS_NextRP ..")" , armsStr, "Make a purchase", materialStr, liquidStr, equipStr, "Cancel")
 		if c == 0 then --■AM-ARMSコントロール
 			if AMTS_Rank < 5 then
-				cmsg("このコマンドはRank5から利用可能です", "red")
+				cmsg("This requires at least store rank 5", "red")
 				return
 			end
-			ARMSMenu("AM-ARMSコントロール(所有ポイント:" .. AMTS_Point .. "/Rank:" .. AMTS_Rank .. "/Next:" .. AMTS_NextRP .. ")")
+			ARMSMenu("AM-ARMS controller (Points::" .. AMTS_Point .. "/Rank:" .. AMTS_Rank .. "/Next:" .. AMTS_NextRP .. ")")
 			
 		elseif c == 1 then --■物資転送
 			p3menu(reward_list,item2)
@@ -174,12 +174,12 @@ function amts_reciver(item2, active)
 			
 		elseif c == 3 then --■液体転送
 			if AMTS_Rank < 3 then
-				cmsg("このコマンドはRank3から利用可能です", "red")
+				cmsg("This requires at least store rank 3", "red")
 				return
 			end
-			local no = LiquidMenu("何を転送しますか？(所有ポイント:" .. AMTS_Point .. "/Rank:" .. AMTS_Rank .. "/Next:" .. AMTS_NextRP ..")")
+			local no = LiquidMenu("What liquid do you wish transfered？(Points::" .. AMTS_Point .. "/Rank:" .. AMTS_Rank .. "/Next:" .. AMTS_NextRP ..")")
 			if no == #l_name then
-				msg("キャンセルしました")
+				msg("Transaciton cancelled")
 				return
 			end
 			
@@ -199,7 +199,7 @@ function amts_reciver(item2, active)
 			
 		elseif c == 4 then --■装備転送
 			if AMTS_Rank < 2 then
-				cmsg("このコマンドはRank2から利用可能です", "red")
+				cmsg("This requires at least store rank 2", "red")
 				return
 			end
 			p3menu(equip_list,item2)
@@ -223,7 +223,7 @@ end
 
 -- ■3要素配列用転送メニュー(手抜き用)
 function p3menu(list,item2)
-	local title = "何を転送しますか？(所有ポイント:" .. AMTS_Point .. "/Rank:" .. AMTS_Rank .. "/Next:" .. AMTS_NextRP .. ")"
+	local title = "What do you wish sent? (Points:" .. AMTS_Point .. "/Rank:" .. AMTS_Rank .. "/Next:" .. AMTS_NextRP .. ")"
 	local name, rank, cost = RewardListMenu(title,list)
 	if name ~= nil then
 		ReceiveItem(item(name,1),cost,rank,item2.charges)
@@ -237,38 +237,38 @@ function amts_transmitter(item2, active)
 	Load_AMTS_Point()
 	EditRP(0)
 	
-	local c = CreateMenu("AMTS-納品(所有ポイント:" .. AMTS_Point .. "/Rank:" .. AMTS_Rank .. "/Next:" .. AMTS_NextRP ..")" , "納品する","納品アイテムリスト", "AMTSパッケージングシステムをもらう", "やめる")
+	local c = CreateMenu("AMTS-Delivery (Points owned:" .. AMTS_Point .. "/Rank:" .. AMTS_Rank .. "/Next:" .. AMTS_NextRP ..")" , "Send","Exchange list", "Request AMTS matter compressor", "Cancel")
 	if c == 0 then
-		local no = ItemListMenu("何を納品しますか？")
+		local no = ItemListMenu("What do you wish to exchange for points?")
 		if no == nil or no == "cancel" then
-			msg("納品をキャンセルしました")
+			msg("Transaction cancelled.")
 			return
 		end
 		
 		SendItem(GetInvItem(pac_name[no]),pac_point[no],item2.charges)
 	elseif c == 1 then
-		msg("納品リストを確認します。")
+		msg("Check the purchase list")
 		viewNouhin()
 	elseif c == 2 then
 		if item2.charges == 0 then
-			cmsg("チャージ量が不足しています", "light_red")
+			cmsg("Insufficient charges remaining", "light_red")
 			return
 		end
 		
 		player:i_add(item("kawaii_amts_pacsys",1))
-		cmsg("AMTSパッケージングシステムをもらいました", "cyan")
+		cmsg("Received an AMTS packaging system", "cyan")
 		EditCharges(DNt, -1)
 	elseif c == 3 then
 	
 	else
-		cmsg("[kawaii error!!]選択肢IDが範囲外です(T1)", "red")
+		cmsg("Choice ID out of range!", "red")
 	end
 end
 
 -- ■AMTSインストールキット(外箱のメモを読む)
 function amts_kit(item, active)
 	game.popup(gText("boxmemo"))
-	cmsg("おしゃれな箱を開けました", "light_green")
+	cmsg("You opened the fancy box", "light_green")
 	player:i_add(item("kawaii_amts_box2",1))
 	player:i_add(item("kawaii_amts_manual",1))
 	player:i_rem(item)
@@ -276,7 +276,7 @@ end
 
 -- ■AMTSインストールキット(施術)
 function amts_kit2(item, active)
-	cmsg("AMTSデバイスを埋め込みました。", "light_green")
+	cmsg("AMTS device implanted.", "light_green")
 	player:set_value("Kawaii_AMTS_Active", "true")
 	player:i_add(item("kawaii_amts_reciver", 1))
 	player:i_add(item("kawaii_amts_transmitter", 1))
@@ -296,13 +296,13 @@ end
 -- ■アイテム受取
 function ReceiveItem(item,cost,rank,charges)
 	if charges == 0 then
-		cmsg("チャージ量が不足しています", "light_red")
+		cmsg("Insufficient power remaining", "light_red")
 		return
 	elseif rank > AMTS_Rank then
-		cmsg("ランクが不足しています", "light_red")
+		cmsg("Insufficient store rank", "light_red")
 		return
 	elseif cost > AMTS_Point then
-		cmsg("ポイントが不足しています", "light_red")
+		cmsg("Insufficient points available", "light_red")
 		return
 	end
 	
@@ -341,25 +341,25 @@ function ReceiveItem(item,cost,rank,charges)
 	end
 	EditPoint(-(cost))
 	EditCharges(DNr,-1)
-	msg("<color_pink>[-" .. cost .. "Point]</color>" .. dname .. " を転送しました。(所有ポイント:" .. AMTS_Point .. ")")
+	msg("<color_pink>[-" .. cost .. "Point]</color>" .. dname .. " has been successfully transfered. (Points:" .. AMTS_Point .. ")")
 end
 
 -- ■アイテム納品
 function SendItem(item,point,charges)
 	if charges == 0 then
-		cmsg("チャージ量が不足しています", "light_red")
+		cmsg("Insufficient charges remaining", "light_red")
 		return
 	end
 	EditPoint(point)
 	EditCharges(DNt, -1)
 	player:i_rem(item)
-	msg("<color_pink>[+" .. point .. "Point]</color>" .. item:display_name() .. " を納品しました。(所有ポイント:" .. AMTS_Point .. ")")
+	msg("<color_pink>[+" .. point .. "Point]</color>" .. item:display_name() .. " has been sent in exchange for points. (Points:" .. AMTS_Point .. ")")
 	EditRP(1)
 end
 
 -- ■納品アイテム確認用リストを表示
 function viewNouhin()
-	local text = "         <<  納品アイテムリスト  >>     \n"
+	local text = "         <<  Delivery Item List  >>     \n"
 	for i in pairs(pac_name) do
 		text = text .. "<color_pink>[" .. pac_point[i] .. "Point]</color> " .. item(pac_name[i],1):display_name() .. "\n"
 	end
@@ -412,9 +412,9 @@ function EditRP(point)
 	AMTS_NextRP = reqRP[i] - rp2
 	
 	if point > 0 and AMTS_Rank > oldRank then
-		cmsg("AMTSランクが上がりました!!(Rank" .. i .. ")", "light_green")
-		local apt = "自動獲得ポイント" .. ap[math.ceil(i/2)] .. "->" .. ap[math.ceil(i/2)+1]
-		local bonus = "転送アイテム追加 "
+		cmsg("Your AMTS rank has increased!(Rank" .. i .. ")", "light_green")
+		local apt = "Bonus points earned" .. ap[math.ceil(i/2)] .. "->" .. ap[math.ceil(i/2)+1]
+		local bonus = "You have received a free bonus item as a reward"
 		--参考用:ap = { 0, 100, 200, 300, 500, 1000 }
 		
 		if i == 2 then
@@ -457,7 +457,7 @@ function EditRP(point)
 	function GiveItem(itemstr)
 		local item = item(itemstr,1)
 		player:i_add(item)
-		msg("<color_light_green>ランクアップボーナス:</color>" .. item:display_name())
+		msg("<color_light_green>Rank up bonus:</color>" .. item:display_name())
 	end
 
 	player:set_value("Kawaii_AMTS_RP", tostring(AMTS_RP))
@@ -524,7 +524,7 @@ function ARMSMenu(title)
 
 	if no < #name then
 		if no == 0 then
-			msg("AMDS内のEVEマガジンが装填されました。")
+			msg("The Eve magazine was loaded via AMTS")
 			local itemstr = "kawaii_eve_mag"
 			local ammo = item("kawaii_308AM",1)
 			local item = GetInvItem(itemstr)
@@ -675,11 +675,11 @@ function ItemListMenu(title)
 		end
 	end
 	if count == 0 then
-		cmsg("納品アイテムがありません。", "red")
+		cmsg("No item has been received.", "red")
 		return
 	end
 	
-	menu:addentry("やめる")
+	menu:addentry("Quit")
 	menu:query(true)
 	choice = menu.selected
 	if #has_list < choice then
@@ -696,7 +696,7 @@ function EditPoint(point)
 	
 	if AMTS_Point == item:ammo_capacity() or AMTS_Point > item:ammo_capacity() then
 		AMTS_Point = item:ammo_capacity()
-		msg("AMTSポイントの上限に達しました。")
+		msg("AMTS point limit reached.")
 	end
 	
 	AMTS_Point = math.ceil(AMTS_Point)
@@ -717,22 +717,22 @@ function EditCharges(name, point)
 	local item = GetInvItem(name)
 	if point > 0 then
 		if math.floor(item.charges) < item:ammo_capacity() then
-			msg(item:display_name() .. " のチャージ量+" .. point .. " =")
+			msg(item:display_name() .. " Points increased by +" .. point .. " =")
 			item.charges = item.charges + point
 			if item.charges > item:ammo_capacity() then
 				item.charges = item:ammo_capacity()
-				msg(item:display_name() .. "のチャージ量が上限に達しました。")
+				msg(item:display_name() .. " The points are at maximum capacity.")
 			end
 			msg(item:display_name())
 		else
-			msg(item:display_name() .. " はこれ以上チャージできません。")
+			msg(item:display_name() .. " The points are at maximum capacity.")
 		end
 	end
 	if point < 0 then
 		if math.floor(item.charges)  > 0 then
 			item.charges = item.charges + point
 		else
-			msg(item:display_name() .. " のチャージ量が不足しています。")
+			msg(item:display_name() .. " There are insufficient points available.")
 		end
 	end
 	
@@ -847,76 +847,60 @@ function gText(name)
 	--本文22行が限界っぽい
 	if name == "boxmemo" then
 		text = _[[
-		                   <color_cyan>(箱に貼り付けられた紙)</color>
-		  この度は、A&M Transport System βテスターにご応募いただき
-		  ありがとうございました。
+		                   <color_cyan>This note was pasted on the box.</color>
+		  You have been accepted into the A&M Transport System beta test.
+		  Thank you for your interest in supporting our company.
 		  
-		  厳正な抽選の結果、ご当選されましたので
-		  サービスをご利用頂く上で必要な製品一式をお届けさせて頂きました。
+		  In this case is everything needed to use our product.
 		  
-		  サービスのご利用を開始して頂くには
-		  同梱のインプラント施術キットを
-		  取扱説明書をよくお読み頂いた上でご使用ください。
+		  In order to use this device, please apply the pre-packaged implant.
+		  After that, please read the manual carefully.
 		  
-		  また、事前にご説明させて頂いた通り
-		  インプラント施術をした段階で利用規約に同意して頂いた物と
-		  させて頂きますので、ご了承ください。
-		  
-		  A&M Transport System が、お客様にとって
-		  良いサービス体験になりますようお祈り致します。
+		  We hope you enjoy using the A&M Transport System
 		  
 		  <color_light_blue>A&M Transport System</color>
-		  <color_light_blue>βテスターサポートデスク:</color>support@AM@AMTS
+		  <color_light_blue>Beta Test Support Desk:</color>support@AM@AMTS
 		]]
 	end
 	if name == "manual_1" then
 		text = _[[
 			 ---+---+---+---+   A&M Transport System<color_cyan>(AMTS)</color>   +---+---+---+--- 
-			                         <color_cyan>(取扱説明書)</color> 1/3
+			                         <color_cyan>(Owner's Manual)</color> 1/3
 			 
-			<color_pink>【概要】</color>
-			    体内に極小の制御デバイスを埋め込む事で
-			    A&M拠点と双方向の次元転送が可能になるシステム一式、及び
-			    インプラント施術キットです。
-			<color_pink>【施術方法】</color>
-			    1,同梱のシリンダーの2カ所の電源ボタン(図1)を同時に5秒以上押し
-			       電源を入れる
+			<color_pink>[Overview]</color>
+			    First you must install the implant kit as per instructions, so you can 
+				be connected to the Alice & Maria Dimensional Transfer network.
+
+			<color_pink>[Implant Installation]</color>
+			    1. Press both power buttons (Fig 1) of the enclosed cylinder simutaniously.
+			    2. Wait until the display on the cylinder shows [Ready]
+				3. Apply the marked side of the cylinder to the upper arm (Fig 2)
+				4. Press the button on top of the cylinder to initialize the process.
 			 
-			    2,シリンダーの表示部が[準備完了]になるまで待つ
-			       (30秒程度)
-			 
-			    3,シリンダーの▼マーク側を二の腕(図2)にあてる
-			       ※その他の場所でも問題ありませんが
-			         当社検証により二の腕が最も不快感が少ない結果となり
-			         ましたので、二の腕への施術をお勧めします
-			 
-			    4,そのままの状態でシリンダー上部のボタン(図3)を押すと
-			       ナノマシンが体内に入り、システムを構築・初期化します
 			 ---+---+---+---+---+---+---+--------+---+---+---+---+---+---+--- 
 		]]
 	end
 	if name == "manual_2" then
 		text = _[[
 			 ---+---+---+---+   A&M Transport System<color_cyan>(AMTS)</color>   +---+---+---+--- 
-			                         <color_cyan>(取扱説明書)</color> 2/3
+			                         <color_cyan>(Owner's Manual)</color> 2/3
 			 
-			<color_pink>【使用方法】</color>
-			    各操作は脳波コントロールにより行います
-			     ※本書ではVoiceImageControl(VIC)と記述します
-			    頭の中で発音する様にイメージする事でコマンドを指示して下さい
-			    デフォルト状態では各コマンドに対して頭の中で
-			    音声案内が再生されますので、それに従って習得して頂くことを
-			    お勧め致します。
-			<color_pink>【基本操作】</color>(VoiceImageControlコマンド/ステート)
-			    <color_light_blue>[チュートリアル]/[ON,OFF]</color> : チュートリアル案内の切り替え
-			    <color_light_blue>[ヘルプ]/[検索単語]</color> : 使用方法を音声案内で確認する
-			    <color_light_blue>[転送]/[各転送物の視覚イメージかID]</color> : 各物資の双方向転送
-			     ※受信時は手のひらの上に存在するイメージをして下さい
-			     ※大きな物の送信時は専用パッケージをご利用ください(P51)
-			     ※詳細な使い方やコツなどはP42(転送)をお読み下さい
-			    <color_light_blue>[時刻]/[ON/OFF]</color> : 時刻を視界に表示(P5)
-			    <color_light_blue>[温度]/[ON/OFF]</color> : 温度を視界に表示(P5)
-			    <color_light_blue>[タイマー]/[時間]</color> : タイマーを視界に表示(P5)
+			<color_pink>[How to use the system]</color>
+				The entire system is controlled by a neutral interface.
+				You can give commands simply by thinking about it.
+				By default, an audio response will play in response to commands
+				to assist in verifying the system had properly received your request.
+			<color_pink>[Basic Operation]</color>
+			    <color_light_blue>[Tutorial]/[ON,OFF]</color> : Toggles tutorial guidance
+			    <color_light_blue>[Help]/[Search]</color> : Confirm usage with voice commands
+			    <color_light_blue>[Transfer]/[ ID ]</color> : Two-way item transfer
+			     - Please think of the product ID, or imagine the picture of the item 
+				   to place an order with the system
+				 - Please use the matter compression system to send large items (p. 51)
+				 - Please read p. 42 for detailed usage and tips
+			    <color_light_blue>[Time]/[ON/OFF]</color> : Displays time (p. 5)
+			    <color_light_blue>[Thermometer]/[ON/OFF]</color> : Displays temperature (p. 5)
+			    <color_light_blue>[Alarm]</color> : Display alarm in view (p. 5)
 				 
 			 ---+---+---+---+---+---+---+--------+---+---+---+---+---+---+--- 
 		]]
@@ -924,15 +908,15 @@ function gText(name)
 	if name == "manual_3" then
 		text = _[[
 			 ---+---+---+---+   A&M Transport System<color_cyan>(AMTS)</color>   +---+---+---+--- 
-			                         <color_cyan>(取扱説明書)</color> 3/3
+			                         <color_cyan>(Owner's Manual)</color> 3/3
 			     
-			     <color_light_blue>[距離]/[対象物をポインティングする]</color> : 距離を測る(P4)(P7)
-			     <color_light_blue>[ガジェット]/[ファイル]</color> : ガジェットを視界に表示(P4)(P8)
-			     <color_light_blue>[メモ]/[テキスト]</color> : テキストを保存(P4)(P9)
-			     <color_light_blue>[カメラ]/[範囲指示]</color> : 視界イメージを保存(P11)
-			     <color_light_blue>[録音]/[開始/停止]</color> : 音声を保存(P16)
-			     <color_light_blue>[計算]/[計算式]</color> : 計算をする(P18)
-			     <color_light_blue>[ファイルを開く]/[ファイル]</color> : 画像や音声などを表示(P4)(P19)
+			 <color_light_blue> [Distance] / [Pointing object] </ color>: Measure the distance (p. 4)
+			 <color_light_blue> [Gadget] / [File] </ color>: Display gadget in view (p. 8)
+			 <color_light_blue> [Memo] / [text] </ color>: Save text (p. 9)
+			 <color_light_blue> [Camera] / [Range instruction] </ color>: Save the view image (p. 11)
+			 <color_light_blue> [Record] / [Start / Stop] </ color>: Save audio (p. 16)
+			 <color_light_blue> [calculation] / [calculation formula] </ color>: Calculator (p. 18)
+			 <color_light_blue> [Open File] / [File] </ color>: Display images, sounds, etc. (p. 20)
 			 
 				 
 			 ---+---+---+---+---+---+---+--------+---+---+---+---+---+---+--- 
